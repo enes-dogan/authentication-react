5fc039c1c72435504880778803e9d7f702b6a5b1
enes-dogan <enesdogan210@gmail.com>
2024-01-20 03:14:40 +0300

    adding 1h expiration to token since token will be invalid after 1h set by backend server

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,30 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation.tsx';
+import { getTokenDuration } from '../util/auth.ts';
 
 export default function RootLayout() {
+  const token = useLoaderData() as string;
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'POST' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'POST' });
+    }, tokenDuration);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
